1a62fbf5ff8d3b36ba558e9ad77c4bdd3315c54e
RyanBarry-STUDENT <RyanBarry-STUDENT@S-6P1H504-SL23>
2026-05-21 13:08:39 +0100

    up to part (e) done

---
+++ May2026_OODExam/Models/Member.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May2026_OODExam.Models
+{
+    public class Member
+    {
+        // properties for Member class
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ContactNumber { get; set; }
+        public string MembershipType { get; set; }
+
+        //foerign key to TrainingSession class
+        public virtual List<TrainingSession> TrainingSessions { get; set; }
+
+        // constructor for Member class
+        public Member()
+        {
+            
+        }
+
+        // methods for Member class
+
+        public override string ToString()
+        {
+            return $"{FirstName} {Surname} - {MembershipType}";
+        }
+    }
+}
---
+++ May2026_OODExam/Models/TrainingSession.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May2026_OODExam.Models
+{
+    public class TrainingSession
+    {
+
+        // properties for TrainingSession class
+        public int SessionId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public string SessionType { get; set; }
+        public int DurationMinutes { get; set; }
+        public string CoachNotes { get; set; }
+
+        // foreign key to Member class
+        public virtual int MemberId { get; set; }
+
+        public virtual Member Member { get; set; }
+
+        // constructor for TrainingSession class
+        public TrainingSession()
+        {
+            
+        }
+
+        // methods for TrainingSession class
+
+        public override string ToString()
+        {
+            return $"{SessionType} on {SessionDate.ToShortDateString()} for {DurationMinutes} minutes";
+        }
+    }
+}
